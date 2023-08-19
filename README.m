cccba07aaca55c5703761f20396142ca25a42d05
Shibuuuo <142652254+Shibuuuo@users.noreply.github.com>
2023-08-19 11:56:20 +0530

    HTML

---
+++ README.m
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Shivansh</title>
+    <body>
+        <h1 style="color:blue">Hi!</h1>
+        <p>I am Shivansh Mishra</p><br>
+        <p>I am 12yrs old</p><br>
+        <P>I am from Bihar</p><br>
+        <p>I am in Class8</p>
+            <h2 style="color:limegreen">More About Me</h2>
+            <P>I am a HTML/CSS programmer.I do HTML most likely everyday</p><Br>
+            <p>I use Visual Studio Code Editor for doing HTML</p><br>
+            <p>I use Windows10 Education for doing HTML</p>
+            <h3 style="color:crimson">What Will I Do After HTML?</h3>
+            <ul type="disc">
+                <li>I will start doing Python</p>
+                <li>I will start developing Softwares
+                </ul>
+                <h4 style="color:plum">What Are My Favourite Subjects?</h4>
+                <ol type="I">
+                    <li>Computers</li>
+                    <li>Computers</li>
+                    <li>Computers</li>
+                </ol>
+                <p>YES,MY FAVOURITE SUBJECT IS COMPUTERS</p>
+                <br>
+                <h5>PLEASE GIVE ME A FEEDBACK</h5>
+                <form>
+                    <input type="checkbox" value="Yes">Yes<br>
+                    <input type="checkbox" value="No">No
+                </form>
+                <select>
+                    <option value="sad">1 STAR</option>
+                    <option value="sad">2 STAR</option>
+                    <option value="sad">3 STAR</option>
+                    <option value="sad">4 STAR</option>
+                    <option value="sad">5 STAR</option>
+                </select>
+                <form>
+                    Feedback:<br>
+                    <input type="text">
+                </form>
+                <br>
+                <a href="file:///C:/Users/DELL/Desktop/feedback.html">Submit</a>
+                </body>
+                </head>
+                </html>
+                
+                
+            
